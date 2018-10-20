1b509fdc5eadc3bf1d68720a602edf9e5b489bc8
Ayesha Khan <akhan1231@hotmail.com>
2018-10-20 13:01:52 -0700

    Initial start of sign up

---
+++ Food Wheel/SignUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewController.swift
+//  Food Wheel
+//
+//  Created by Ayesha Khan on 10/20/18.
+//  Copyright © 2018 San Jose State University. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class SignUpViewController {
+    
+}
