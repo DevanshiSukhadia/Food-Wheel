cdb03de454d0fe773bdf8fe6e066438ce1371b1c
np1goal <np1goal@gmail.com>
2018-11-17 13:02:03 -0800

    Added screens and fucntions
    Screens + Navigation + Spin wheel function + Zip code fucntion

---
+++ Food Wheel/UserHistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserHistoryViewController.swift
+//  Food Wheel
+//
+//  Created by Nishtha Patel on 11/16/18.
+//  Copyright © 2018 San Jose State University. All rights reserved.
+//
+
+import UIKit
+
+class UserHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
