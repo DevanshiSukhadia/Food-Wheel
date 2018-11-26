81c5cc97f315b5d6ae6f1a1706a70fde673bd14b
Devanshi <sukhadia.devanshi@gmail.com>
2018-11-25 20:29:44 -0800

    finished the pop up

--- Food Wheel/FoodSpinnerViewController.swift
+++ Food Wheel/FoodSpinnerViewController.swift
@@ -11,18 +11,23 @@ import UIKit
 class FoodSpinnerViewController: UIViewController {
 
     @IBOutlet weak var Food: UIButton!
-    
     @IBOutlet weak var GreekFood: UIButton!
     
+    var greekFoodShuffleButtonCenter: CGPoint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        greekFoodShuffleButtonCenter = GreekFood.center
+        GreekFood.center = Food.center
         // Do any additional setup after loading the view.
     }
     
 
     @IBAction func FoodClicked(_ sender: UIButton) {
-        
+        UIView.animate(withDuration: 0.3) {
+            //animation
+            self.GreekFood.center = self.greekFoodShuffleButtonCenter
+        }
     }
     
 
