d46097d86f8afb000c5ed986d2bb10885d807033
Devanshi <sukhadia.devanshi@gmail.com>
2018-11-25 21:18:26 -0800

    finished wheel for coffee and food

--- Food Wheel/CoffeeSpinnerViewController.swift
+++ Food Wheel/CoffeeSpinnerViewController.swift
@@ -9,14 +9,35 @@
 import UIKit
 
 class CoffeeSpinnerViewController: UIViewController {
+    @IBOutlet weak var Peets: UIButton!
+    @IBOutlet weak var Starbucks: UIButton!
+    @IBOutlet weak var Philz: UIButton!
+    @IBOutlet weak var coffee: UIButton!
+    
+    var PeetsFoodShuffleButtonCenter: CGPoint!
+    var StarbucksFoodShuffleButtonCenter: CGPoint!
+    var PhilzFoodShuffleButtonCenter: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        PeetsFoodShuffleButtonCenter = Peets.center
+        StarbucksFoodShuffleButtonCenter = Starbucks.center
+        PhilzFoodShuffleButtonCenter = Philz.center
+        
+        Peets.center = coffee.center
+        Starbucks.center = coffee.center
+        Philz.center = coffee.center
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func click(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.Peets.center = self.PeetsFoodShuffleButtonCenter
+            self.Starbucks.center = self.StarbucksFoodShuffleButtonCenter
+            self.Philz.center = self.PhilzFoodShuffleButtonCenter
+        }
+    }
     /*
     // MARK: - Navigation
 
--- Food Wheel/FoodSpinnerViewController.swift
+++ Food Wheel/FoodSpinnerViewController.swift
@@ -12,13 +12,31 @@ class FoodSpinnerViewController: UIViewController {
 
     @IBOutlet weak var Food: UIButton!
     @IBOutlet weak var GreekFood: UIButton!
+    @IBOutlet weak var IndianFood: UIButton!
+    @IBOutlet weak var ItalianFood: UIButton!
+    @IBOutlet weak var JapaneseFood: UIButton!
+    @IBOutlet weak var MexicanFood: UIButton!
     
     var greekFoodShuffleButtonCenter: CGPoint!
+    var IndianFoodShuffleButtonCenter: CGPoint!
+    var ItalianFoodShuffleButtonCenter: CGPoint!
+    var JapaneseFoodShuffleButtonCenter: CGPoint!
+    var MexicanFoodShuffleButtonCenter: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         greekFoodShuffleButtonCenter = GreekFood.center
+        IndianFoodShuffleButtonCenter = IndianFood.center
+        ItalianFoodShuffleButtonCenter = ItalianFood.center
+        JapaneseFoodShuffleButtonCenter = JapaneseFood.center
+        MexicanFoodShuffleButtonCenter = MexicanFood.center
+
         GreekFood.center = Food.center
+        IndianFood.center = Food.center
+        ItalianFood.center = Food.center
+        JapaneseFood.center = Food.center
+        MexicanFood.center = Food.center
+        
         // Do any additional setup after loading the view.
     }
     
@@ -27,6 +45,10 @@ class FoodSpinnerViewController: UIViewController {
         UIView.animate(withDuration: 0.3) {
             //animation
             self.GreekFood.center = self.greekFoodShuffleButtonCenter
+            self.IndianFood.center = self.IndianFoodShuffleButtonCenter
+            self.ItalianFood.center = self.ItalianFoodShuffleButtonCenter
+            self.JapaneseFood.center = self.JapaneseFoodShuffleButtonCenter
+            self.MexicanFood.center = self.MexicanFoodShuffleButtonCenter
         }
     }
     
