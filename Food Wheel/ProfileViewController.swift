4a77efa264c6f9ba053c768850639b22768497b6
Devanshi <sukhadia.devanshi@gmail.com>
2018-11-26 00:40:18 -0800

    added profile page

---
+++ Food Wheel/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  Food Wheel
+//
+//  Created by Devanshi Sukhadia on 11/26/18.
+//  Copyright © 2018 San Jose State University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController : UIViewController {
+    
+}
