5cd9d0d9426f658123fa5144a78afe60b8f4e0d3
np1goal <np1goal@gmail.com>
2018-11-30 22:19:44 -0800

    Added View Controllers

---
+++ Food Wheel/NewConfirmPwdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewConfirmPwdViewController.swift
+//  Food Wheel
+//
+//  Created by Nishtha Patel on 11/30/18.
+//  Copyright © 2018 San Jose State University. All rights reserved.
+//
+
+import UIKit
+
+class NewConfirmPwdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
