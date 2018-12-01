81ad9c30401a1525abdea7fcd0d9a3d2640b64fd
np1goal <np1goal@gmail.com>
2018-11-30 22:22:58 -0800

    Header file

---
+++ sendEmail_bridge.h
@@ -0,0 +1,13 @@
+//
+//  sendEmail_bridge.h
+//  Food Wheel
+//
+//  Created by Nishtha Patel on 11/30/18.
+//  Copyright © 2018 San Jose State University. All rights reserved.
+//
+
+#ifndef sendEmail_bridge_h
+#define sendEmail_bridge_h
+#include "MailCore.h"
+
+#endif /* sendEmail_bridge_h */
